0abbe4eedb8435320681c9a7ee45779ba46bf285
David Christoffersen <david_christoffersen@live.dk>
2025-11-03 19:38:14 +0100

    Second commit

---
+++ ISBNDBIntegration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ISBNDBIntegration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
